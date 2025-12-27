4d50c047f5270004ef3f3500cfd936e248151e9e
Razmik Kutinava <razmikg1988@gmail.com>
2025-12-27 15:24:37 +0400

    Rebuild web for v15 - location_id from hash

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -41,14 +41,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "3a88f4fd74dac29c98651204c1737a31
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "30c1abb129f486d768aff2359e71329e",
+"flutter_bootstrap.js": "ea04ea6f53091db4e25ff1a2b61abc8a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "f94083f11f2bf3224ca4a13cfc1cc730",
 "/": "f94083f11f2bf3224ca4a13cfc1cc730",
-"main.dart.js": "219347a97a75a0e06fd93f3afdfabb96",
+"main.dart.js": "7e779134c14d9f1974c7a83b543f4ad4",
 "manifest.json": "197c5f831c9aaf48719a6d05b6ed8b42",
 "version.json": "13be261121192953b9dfaec172287b94"};
 // The application shell files that are downloaded before a service worker can
