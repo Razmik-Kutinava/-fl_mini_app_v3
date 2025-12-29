7612303620011b8b1ec9930ae78e797a8fdb2583
Razmik Kutinava <razmikg1988@gmail.com>
2025-12-29 19:00:47 +0400

    Обновлена сборка web - версия 18.2 с исправленным диалогом локации

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "984701852"
+    serviceWorkerVersion: "3785463886"
   }
 });
