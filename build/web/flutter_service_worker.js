9b4402e8dd6334fb99df82fef372104317673810
Razmik Kutinava <razmikg1988@gmail.com>
2026-01-07 19:53:25 +0400

    fix: Update version to 19.7 and rebuild - fix value.includes error
    - Updated all version references to 19.7
    - Rebuilt web application with latest changes
    - Fixed category filtering logic
    - Version: 19.7

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -41,14 +41,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "3a88f4fd74dac29c98651204c1737a31
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "981a18de7dbbb8e3070e726c8c2dc6b3",
+"flutter_bootstrap.js": "635067bfccfb2368f50e0dfc8c1a834b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "69b1cb1eeb71d8c72ac97cb2ecb517ff",
-"/": "69b1cb1eeb71d8c72ac97cb2ecb517ff",
-"main.dart.js": "d5c6b3b45f24d10cd23e5db6320c61a9",
+"index.html": "d2061bebb09c814c2d34ec27b9e82f8c",
+"/": "d2061bebb09c814c2d34ec27b9e82f8c",
+"main.dart.js": "5535c6b02885c8edb8ff1d454384623d",
 "manifest.json": "197c5f831c9aaf48719a6d05b6ed8b42",
 "version.json": "13be261121192953b9dfaec172287b94"};
 // The application shell files that are downloaded before a service worker can
