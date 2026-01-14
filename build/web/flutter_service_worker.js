c49d80307844c4af9190ca7a96857d61264f1e33
Razmik Kutinava <razmikg1988@gmail.com>
2026-01-14 18:27:54 +0400

    build: update web build with GIF fix

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -42,14 +42,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "58e33a17c704871a9073f8a34a98fb14
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "2aae8c52011d8658dcbef58a962ed4b8",
+"flutter_bootstrap.js": "68108e7c43c0a90a6dfc39daffa44c33",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "79f0b32c9aa44718138333aaa6f14cd2",
 "/": "79f0b32c9aa44718138333aaa6f14cd2",
-"main.dart.js": "6499adf60c337234f11c9e8a2c7e7346",
+"main.dart.js": "69528ad39d95826d91b8c71012c4cf12",
 "manifest.json": "197c5f831c9aaf48719a6d05b6ed8b42",
 "version.json": "8c9d65b53ec5b3616fa7efdfbc03b7b1"};
 // The application shell files that are downloaded before a service worker can
