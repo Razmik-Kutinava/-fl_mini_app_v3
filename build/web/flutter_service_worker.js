87a4bdbfddff1adad810adf0027f5c767e7cefde
Razmik Kutinava <razmikg1988@gmail.com>
2026-01-31 21:18:31 +0400

    release: v1.0 - 3-state vertical carousel with scrolls

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "58e33a17c704871a9073f8a34a98fb14
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "ce889ec87beae9f6304b550e645f7ea6",
+"flutter_bootstrap.js": "0b998946c2fbfee50d631fc0d7ef40be",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
